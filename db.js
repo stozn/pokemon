c95000b128a3e872555a57d1f3906fb81f13cd4c
RichardPaulAstley <bobodu08@hotmail.fr>
2017-03-06 20:20:47 +0100

    Update db.js
    DB added with 1G Mega. 
    Need to be updated with sprites later.
    Not available in game for the moment.

--- db.js
+++ db.js
@@ -4831,5 +4831,486 @@ const POKEDEX = [
          "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/mew.gif",
          "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/mew.gif"
       }
-   }
+   },
+//Mega1G
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Venusaur"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "45",
+			"growth rate": "Medium Slow",
+            "hp": "80",
+            "attack": "100",
+            "defense": "123",
+            "sp atk": "122",
+            "sp def": "120",
+            "speed": "80",
+            "types": {
+               "text": "Grass",
+               "href": "https://pokemondb.net/type/grass"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "208"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/venusaur.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/venusaur.gif"
+      }
+   },
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Charizard X"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "45",
+            "growth rate": "Medium Slow",
+            "hp": "78",
+            "attack": "130",
+            "defense": "111",
+            "sp atk": "130",
+            "sp def": "85",
+            "speed": "100",
+            "types": {
+               "text": "Dragon",
+               "href": "https://pokemondb.net/type/dragon"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "209"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/charizard.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/charizard.gif"
+      }
+   },
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Charizard Y"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "45",
+            "growth rate": "Medium Slow",
+            "hp": "78",
+            "attack": "104",
+            "defense": "78",
+            "sp atk": "159",
+            "sp def": "115",
+            "speed": "100",
+            "types": {
+               "text": "Fire",
+               "href": "https://pokemondb.net/type/fire"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "209"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/charizard.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/charizard.gif"
+      }
+   },
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Blastoise"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "45",
+            "growth rate": "Medium Slow",
+            "hp": "79",
+            "attack": "103",
+            "defense": "120",
+            "sp atk": "135",
+            "sp def": "115",
+            "speed": "78",
+            "types": {
+               "text": "Water",
+               "href": "https://pokemondb.net/type/water"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "210"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/blastoise.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/blastoise.gif"
+      }
+   },
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Beedrill"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "45",
+            "growth rate": "Medium Fast",
+            "hp": "65",
+            "attack": "150",
+            "defense": "40",
+            "sp atk": "15",
+            "sp def": "80",
+            "speed": "145",
+            "types": {
+               "text": "Bug",
+               "href": "https://pokemondb.net/type/bug"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "159"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/beedrill.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/beedrill.gif"
+      }
+   },
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Pidgeot"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "45",
+            "growth rate": "Medium Slow",
+            "hp": "83",
+            "attack": "80",
+            "defense": "80",
+            "sp atk": "135",
+            "sp def": "80",
+            "speed": "121",
+            "types": {
+               "text": "Normal",
+               "href": "https://pokemondb.net/type/normal"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "172"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/pidgeot.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/pidgeot.gif"
+      }
+   },   
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Alakazam"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "50",
+            "growth rate": "Medium Slow",
+            "hp": "55",
+            "attack": "50",
+            "defense": "65",
+            "sp atk": "175",
+            "sp def": "105",
+            "speed": "150",
+            "types": {
+               "text": "Psychic",
+               "href": "https://pokemondb.net/type/psychic"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "186"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/alakazam.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/alakazam.gif"
+      }
+   },   
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Slowbro"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "75",
+            "growth rate": "Medium Fast",
+            "hp": "95",
+            "attack": "75",
+            "defense": "180",
+            "sp atk": "130",
+            "sp def": "80",
+            "speed": "30",
+            "types": {
+               "text": "Water",
+               "href": "https://pokemondb.net/type/water"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "164"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/slowbro.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/slowbro.gif"
+      }
+   },   
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Gengar"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "45",
+            "growth rate": "Medium Slow",
+            "hp": "60",
+            "attack": "65",
+            "defense": "80",
+            "sp atk": "170",
+            "sp def": "95",
+            "speed": "130",
+            "types": {
+               "text": "Ghost",
+               "href": "https://pokemondb.net/type/ghost"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "190"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/gengar.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/gengar.gif"
+      }
+   },   
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Kangaskhan"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "45",
+            "growth rate": "Medium Fast",
+            "hp": "105",
+            "attack": "125",
+            "defense": "100",
+            "sp atk": "60",
+            "sp def": "100",
+            "speed": "100",
+            "types": {
+               "text": "Normal",
+               "href": "https://pokemondb.net/type/normal"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "175"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/kangaskhan.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/kangaskhan.gif"
+      }
+   },   
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Pinsir"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "45",
+            "growth rate": "Slow",
+            "hp": "65",
+            "attack": "155",
+            "defense": "120",
+            "sp atk": "65",
+            "sp def": "90",
+            "speed": "105",
+            "types": {
+               "text": "Bug",
+               "href": "https://pokemondb.net/type/bug"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "200"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/pinsir.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/pinsir.gif"
+      }
+   },
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Gyarados"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "45",
+            "growth rate": "Slow",
+            "hp": "95",
+            "attack": "155",
+            "defense": "109",
+            "sp atk": "70",
+            "sp def": "130",
+            "speed": "81",
+            "types": {
+               "text": "Water",
+               "href": "https://pokemondb.net/type/water"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "214"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/gyarados.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/gyarados.gif"
+      }
+   },
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Aerodactyl"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "45",
+            "growth rate": "Slow",
+            "hp": "80",
+            "attack": "135",
+            "defense": "85",
+            "sp atk": "70",
+            "sp def": "95",
+            "speed": "150",
+            "types": {
+               "text": "Rock",
+               "href": "https://pokemondb.net/type/rock"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "202"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/aerodactyl.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/aerodactyl.gif"
+      }
+   },
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Mewtwo X"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "3",
+            "growth rate": "Slow",
+            "hp": "106",
+            "attack": "190",
+            "defense": "100",
+            "sp atk": "154",
+            "sp def": "100",
+            "speed": "130",
+            "types": {
+               "text": "Psychic",
+               "href": "https://pokemondb.net/type/psychic"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "220"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/mewtwo.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/mewtwo.gif"
+      }
+   },
+   {
+      "pokemon": [
+         {
+            "Pokemon": "M-Mewtwo Y"
+         }
+      ],
+      "stats": [
+         {
+            "catch rate": "3",
+            "growth rate": "Slow",
+            "hp": "106",
+            "attack": "150",
+            "defense": "70",
+            "sp atk": "194",
+            "sp def": "120",
+            "speed": "140",
+            "types": {
+               "text": "Psychic",
+               "href": "https://pokemondb.net/type/psychic"
+            }
+         }
+      ],
+      "exp": [
+         {
+            "base exp": "220"
+         }
+      ],
+      "images": {
+         "front": "https://img.pokemondb.net/sprites/black-white/anim/normal/mewtwo.gif",
+         "back": "https://img.pokemondb.net/sprites/black-white/anim/back-normal/mewtwo.gif"
+      }
+   }   
 ];
