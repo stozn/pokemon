d72092e60b2239a1f7f13115220e34c05fa9c2fc
RichardPaulAstley <bobodu08@hotmail.fr>
2018-01-06 00:38:51 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -2297,10 +2297,10 @@ const ROUTES = {
 },
 	Event: {
     event1: {
-      name: 'Old rod'
-    , pokes: ['Magikarp'] 
-    , minLevel: 5
-    , maxLevel: 5
+      name: '#GoodAdviceRPA'
+    , pokes: ['Feraligatr'] 
+    , minLevel: 100
+    , maxLevel: 100
     , unlocked: true
     }
 	}
