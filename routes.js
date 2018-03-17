feb16ad93b537331f7fa7b46ccc06ec8f734c5ec
RichardPaulAstley <bobodu08@hotmail.fr>
2018-03-17 17:54:19 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -2297,10 +2297,17 @@ const ROUTES = {
 },
 	Event: {
     event1: {
-      name: 'Old Rod'
-    , pokes: ['Magikarp'] 
-    , minLevel: 20
-    , maxLevel: 20
+      name: 'White'
+    , pokes: ['Reshiram'] 
+    , minLevel: 70
+    , maxLevel: 70
+    , unlocked: true
+    }
+    , event2: {
+      name: 'Black'
+    , pokes: ['Zekrom'] 
+    , minLevel: 70
+    , maxLevel: 70
     , unlocked: true
     }
 	}
