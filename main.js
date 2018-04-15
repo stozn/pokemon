012238bc99b527c66394569c88b5b0f8a0a976db
RichardPaulAstley <bobodu08@hotmail.fr>
2018-04-16 01:33:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1217,8 +1217,8 @@ const makeCombatLoop = (enemy, player, dom) => {
       attackingTypes[1] && typeEffectiveness(attackingTypes[1], defendingTypes) || 0
      )
   }
-  const eventTimerActive = false
-  const eventTimerExpires = 1523836800
+  const eventTimerActive = true
+  const eventTimerExpires = 1492473600
 
 
   const dealDamage = (attacker, defender, who) => {
