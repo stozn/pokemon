fc82c3cbf137350baf41c299795155e6e97f2e1e
RichardPaulAstley <bobodu08@hotmail.fr>
2018-10-29 23:18:06 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -2297,10 +2297,10 @@ const ROUTES = {
 },
 	Event: {
       event1: {
-      name: 'UB-05 Glutton'
-    , pokes: ['Guzzlord'] 
-    , minLevel: 50
-    , maxLevel: 50
+      name: 'Old Rod'
+    , pokes: ['Magikarp'] 
+    , minLevel: 5
+    , maxLevel: 5
     , unlocked: true
     }
 }
