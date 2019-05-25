d9fa76410d56deba06e92bb494160832c53cd958
RichardPaulAstley <bobodu08@hotmail.fr>
2019-05-25 23:03:42 +0200

    Update routes.js

--- routes.js
+++ routes.js
@@ -2297,10 +2297,10 @@ const ROUTES = {
 },
 	Event: {
       event1: {
-      name: 'Old Rod'
-    , pokes: ['Magikarp'] 
-    , minLevel: 5
-    , maxLevel: 5
+      name: 'Please be Mewtwo'
+    , pokes: ['Mewtwo'] 
+    , minLevel: 70
+    , maxLevel: 70
     , unlocked: true
     }
     , event2: {
