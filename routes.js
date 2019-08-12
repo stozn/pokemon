ba44301d17dddbf6a35b366e078dc15051d92084
RichardPaulAstley <bobodu08@hotmail.fr>
2019-08-12 22:44:28 +0200

    Update routes.js

--- routes.js
+++ routes.js
@@ -2297,10 +2297,10 @@ const ROUTES = {
 },
 	Event: {
       event1: {
-      name: 'Sunny Days'
-    , pokes: ['Groudon'] 
-    , minLevel: 70
-    , maxLevel: 70
+      name: 'Old Rod'
+    , pokes: ['Magikarp'] 
+    , minLevel: 10
+    , maxLevel: 10
 	      
     , unlocked: true
     }
